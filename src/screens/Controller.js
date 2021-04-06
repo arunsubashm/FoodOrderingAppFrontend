7cf9f2efc6e94c0be4b19c8ae2289bceac319861
Rajkumar Lolla <rajkumarlolla@yahoo.com>
2021-04-06 23:12:04 +0530

    Added Checkout Css and Js files to implement the Checkout UI

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Home from '../screens/home/Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Checkout from "../screens/checkout/Checkout";
 
 class Controller extends Component {
 
     constructor() {
       super();
+      this.baseUrl= "http://localhost:8080/api/";
     }
   
     render() {
@@ -13,6 +15,7 @@ class Controller extends Component {
         <Router>
           <div className="main-container">
             <Route exact path='/' render={(props) => <Home {...props} />} />
+            <Route exact path="/checkout" render={props=> <Checkout {...props} baseUrl={this.baseUrl}/>} />
           </div>
         </Router>
       )
